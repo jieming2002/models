b733f8c813eb1c5be552912c5c96c03da60d047f
jieming2002 <jieming2002@gmail.com>
2018-04-22 06:22:46 -0700

    add tinymind dataset

--- research/slim/datasets/dataset_factory.py
+++ research/slim/datasets/dataset_factory.py
@@ -22,12 +22,14 @@ from datasets import cifar10
 from datasets import flowers
 from datasets import imagenet
 from datasets import mnist
+from datasets import tmd001
 
 datasets_map = {
     'cifar10': cifar10,
     'flowers': flowers,
     'imagenet': imagenet,
     'mnist': mnist,
+    'tmd001': tmd001,
 }
 
 
