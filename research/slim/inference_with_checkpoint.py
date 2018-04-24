e1bd4f3302a51b3ff3d6039d8e43e2cc3fe2af26
jieming2002 <jieming2002@gmail.com>
2018-04-24 08:34:02 -0700

    can output single

--- research/slim/inference_with_checkpoint.py
+++ research/slim/inference_with_checkpoint.py
@@ -15,14 +15,18 @@ tf.app.flags.DEFINE_string(
 tf.app.flags.DEFINE_string(
     'model_name', 'cifarnet', 'The name of the architecture to evaluate.')
 
-tf.app.flags.DEFINE_string(
-    'output_file', './output.pb', 'The name of the output_file.')
 # herw we cannot use ~/tmp, must use full directory as /home/skye/tmp
 tf.app.flags.DEFINE_string(
     'checkpoint_path', '/home/skye/tmp/cifarnet-model', 'The directory of checkpoint.')
 
 tf.app.flags.DEFINE_string(
     'pic_path', './test.jpeg', 'The name of the image.')
+
+tf.app.flags.DEFINE_string(
+    'label_path', '', 'The path of the label file.')
+
+tf.app.flags.DEFINE_integer(
+    'default_image_size', 300, 'The size of the image.')
 
 FLAGS = tf.app.flags.FLAGS
 
@@ -50,6 +54,13 @@ if hasattr(network_fn, 'default_image_size'):
 else:
     image_size = FLAGS.default_image_size
 
+with open(FLAGS.label_path, encoding='utf-8') as inf:
+    labels_to_class_names = {}
+    for line in inf:
+        cols = line.strip().split(":")
+        labels_to_class_names[int(cols[0])] = cols[1]
+    # print('labels_to_class_names =', labels_to_class_names)
+
 # begin setting graph
 placeholder = tf.placeholder(name='input', dtype=tf.string)
 # if the image is png, then the channel will be 4, so we must set channels=3
@@ -60,14 +71,18 @@ image = tf.expand_dims(image, 0)
 logit, end_points = network_fn(image)
 
 saver = tf.train.Saver()
-sess = tf.Session()
-# print('FLAGS.checkpoint_path = ', FLAGS.checkpoint_path)
-checkpoint_path = tf.train.latest_checkpoint(FLAGS.checkpoint_path)
-# print('checkpoint_path = ', checkpoint_path)
-# this method is more slow then output method
-saver.restore(sess, checkpoint_path)
-# load the image
-image_value = open(FLAGS.pic_path).read()
-logit_value = sess.run([logit], feed_dict={placeholder:image_value})
-print('logit_value = ', logit_value)
-print('argmax = ', np.argmax(logit_value))
+with tf.Session() as sess:
+    # print('FLAGS.checkpoint_path = ', FLAGS.checkpoint_path)
+    checkpoint_path = tf.train.latest_checkpoint(FLAGS.checkpoint_path)
+    # print('checkpoint_path = ', checkpoint_path)
+    # this method is more slow then output method
+    saver.restore(sess, checkpoint_path)
+    # load the image
+    image_value = open(FLAGS.pic_path, mode='rb').read()
+
+    logit_value = sess.run([logit], feed_dict={placeholder:image_value})
+    print('logit_value = ', logit_value)
+    argmax = np.argmax(logit_value)
+    print('argmax = ', argmax)
+    class_name = labels_to_class_names[argmax]
+    print('class_name = ', class_name)
