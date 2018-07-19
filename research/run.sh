5267f0d80f5456246cc955b6f9b5bedfd3fe9e91
fp-shengkaitang <shengkai.tang@funplus.com>
2018-07-19 10:25:28 +0800

    config 13w

--- research/run.sh
+++ research/run.sh
@@ -17,7 +17,7 @@ checkpoint_dir=$train_dir
 eval_dir=$output_dir/eval
 
 # config文件
-config=faster_rcnn_inception_resnet_v2_ehualu8.config
+config=faster_rcnn_resnet101_ehualu5.config
 pipeline_config_path=$output_dir/$config
 
 # 先清空输出目录，本地运行会有效果，tinymind上运行这一行没有任何效果
@@ -29,7 +29,7 @@ cp $dataset_dir/$config $pipeline_config_path
 
 # 每个 epoch 训练步骤总数 = 训练样本总量 / batch_size
 # step_num=41388
-step_num=75000
+step_num=65000
 
 for i in {0..1} # for 循环中的代码执行 5 次，这里的左右边界都包含，也就是一共训练500个step，每100step验证一次
 do
